305fc326f51973f3252659277f0f5db11871937b
judeboy <judeeboyy@gmail.com>
2025-11-14 13:21:32 -0500

    fix confetti animation

--- src/hooks/useConfetti.ts
+++ src/hooks/useConfetti.ts
@@ -4,17 +4,17 @@ import type { Streak } from "../types";
 
 export function useConfetti(streak: Streak) {
     const [runConfetti, setRunConfetti] = useState(false);
+
     useEffect(() => {
         if (!streak) return;
 
-        if (streak.streak_incremented) {
+        if (streak.streak_incremented === true) {
             setRunConfetti(true);
 
-            const timeout = setTimeout(() => {
+            setTimeout(() => {
                 setRunConfetti(false);
-            }, 5_000);
+            }, 10_000);
 
-            return () => clearTimeout(timeout);
         }
     }, [streak?.streak_incremented])
 
