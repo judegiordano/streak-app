9b5900ab3ac7c52ee953e6d0bf4f251d57e9c3c5
judeboy <judeeboyy@gmail.com>
2025-11-14 00:06:39 -0500

    default to streak of 1

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -24,8 +24,8 @@ pub struct MyConfig {
 impl Default for MyConfig {
     fn default() -> Self {
         MyConfig {
-            streak: 0,
-            longest_streak: 0,
+            streak: 1,
+            longest_streak: 1,
             last_streak_date: Local::now(),
         }
     }
